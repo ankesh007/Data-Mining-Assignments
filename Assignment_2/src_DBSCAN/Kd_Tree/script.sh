c1b222be9110b30f8ff9c9c33b3f3ebe38d935e0
ankesh007 <ankeshgupta007@gmail.com>
2018-09-20 15:56:46 +0530

    Reorganized dir structure

--- Assignment_2/src_DBSCAN/Kd_Tree/script.sh
+++
@@ -1,9 +0,0 @@
-time ./FP_Tree ../Dataset/retail.dat 5 new_output5.txt
-time ./FP_Tree ../Dataset/retail.dat 1 new_output1.txt
-time ./FP_Tree ../Dataset/retail.dat 0.1 new_output01.txt
-time ./FP_Tree ../Dataset/retail.dat 0.08 new_output008.txt
-
-colordiff new_output008.txt output008.txt
-colordiff new_output01.txt output01.txt
-colordiff new_output1.txt output1.txt
-colordiff new_output5.txt output5.txt
